bff298ee2042c4a76622c02c7455883924928484
Cory Wandling <cory@marblesit.com>
2016-04-16 17:16:22 -0500

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # set -x
+
+# Install docker
 wget https://get.docker.com
 mv index.html getdocker.sh
 chmod a+x getdocker.sh
 ./getdocker.sh
 sudo usermod -aG docker `whoami`
 rm ./getdocker.sh
-echo !!!! Remember to logout and log back in !!!!!
 
-sudo -i
-curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m`> /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-exit
+#Install docker-compose
+sudo bash -c "curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m`> /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo !!!! Remember to logout and log back in !!!!!

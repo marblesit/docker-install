b120ab03b6edfc5be73c208b018867e232efd37d
Cory Wandling <cory@marblesit.com>
2016-04-16 17:01:17 -0500

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -7,3 +7,8 @@ chmod a+x getdocker.sh
 sudo usermod -aG docker `whoami`
 rm ./getdocker.sh
 echo !!!! Remember to logout and log back in !!!!!
+
+sudo -i
+curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m`> /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+exit

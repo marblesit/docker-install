222565b99c96d182395b93dea3b9dec452178401
Cory Wandling <cory@marblesit.com>
2016-04-28 04:52:52 +0000

    Enhance message

--- docker-install.sh
+++ docker-install.sh
@@ -13,4 +13,9 @@ rm ./getdocker.sh
 sudo bash -c "curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m`> /usr/local/bin/docker-compose"
 sudo chmod +x /usr/local/bin/docker-compose
 
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+echo !!!!                                    !!!!!
 echo !!!! Remember to logout and log back in !!!!!
+echo !!!!                                    !!!!!
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
